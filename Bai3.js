ca4e0b4a517bb2399adb616f7c8649bc6c89c428
kimtran5566 <lktran5566@gmail.com>
2023-11-22 19:21:40 +0700

    new update

--- Bai3.js
+++ Bai3.js
@@ -1,4 +1,4 @@
-function student (name, age, address, email, phone){
+/*function student (name, age, address, email, phone){
     this.name = name;
     this.age = age;
     this. addess = address;
@@ -10,4 +10,20 @@ const A = new student("Tran Van A", 20, "Ha Noi", "a@gmail.com", 123);
 const B = new student("Tran Van B", 21, "Ha Noi", "b@gmail.com", 456);
 const C = new student("Tran Van C", 22, "Ha Noi", "c@gmail.com", 789)
 
-console.table([A, B, C]);
+console.table([A, B, C]); */
+
+const JSON = [
+    {"name: ": "Nguyen Van A",
+    "age: ": 21,
+    "address: ": "Ha Noi",
+    "email: ": "a@gmail.com",
+    "phone: ": 123},
+
+    {"name: ": "Tran Van B", 
+    "age: ": 21, 
+    "address: ": "Ha Noi", 
+    "email: ": "b@gmail.com", 
+    "phone: ": 456},
+];
+
+console.table(JSON)
